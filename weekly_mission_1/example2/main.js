d5e5a74ef7bf64d6e386230497ed13ad42082ac7
pe-ca <mr.gloser1@gmail.com>
2022-04-15 12:51:47 -0500

    agregado codigo de invocacion

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,8 @@
+// node main.js
+
+// Se hace referencia al archivo logger
+const logger = require('./logger')
+
+// Se invocan las funciones expuesta del archivo logger
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
