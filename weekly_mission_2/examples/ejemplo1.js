4c9c3d4a6b72529a887974f9ecb2d846ec5c43c9
pe-ca <mr.gloser1@gmail.com>
2022-04-15 18:04:57 -0500

    Codigo de ejemplo con comentarios

---
+++ weekly_mission_2/examples/ejemplo1.js
@@ -0,0 +1,12 @@
+// Ejemplo 1: Crear un objeto
+const myObjetc = {} // Esto es un objeto vacío
+console.log("Ejemplo 1: Crear un objeto vacío")
+console.log(myObjetc)
+
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    name: "Carlo",
+    age: 27
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObjetc2) //muestra las propiedades del objeto
