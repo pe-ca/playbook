5e65ec232a71c88530a7aa03ecc669320528342a
pe-ca <mr.gloser1@gmail.com>
2022-04-15 18:13:51 -0500

    separacion de ejemplos 1 y 2

--- weekly_mission_2/examples/ejemplo1.js
+++ weekly_mission_2/examples/ejemplo1.js
@@ -2,11 +2,3 @@
 const myObjetc = {} // Esto es un objeto vacío
 console.log("Ejemplo 1: Crear un objeto vacío")
 console.log(myObjetc)
-
-// Ejemplo 2: Crear un objeto con propiedades
-const myObjetc2 = {
-    name: "Carlo",
-    age: 27
-}
-console.log("Ejemplo 2: Crear un objeto con propiedades")
-console.log(myObjetc2) //muestra las propiedades del objeto
