ca25c1a1087c8dd3bc438ad5371df0e0f8b40c2b
pe-ca <mr.gloser1@gmail.com>
2022-04-15 12:49:43 -0500

    se agregan notas y un ejemplo de una funcion no accesible desde otro archivo

--- weekly_mission_1/example2/logger.js
+++ weekly_mission_1/example2/logger.js
@@ -1,5 +1,8 @@
 // logger.js
 
+// Al agregar la palabra "exports" a una variable hace que el contenido de
+// esta sea accesible desde algun archivo que la invoque siempre
+
 // Esta es una función que se guardara en este módulo como info
 exports.info = (message) => {
     console.log(`info: ${message}`)
@@ -10,8 +13,13 @@ exports.verbose = (message) => {
     console.log(`verbose: ${message}`)
 }
 
+// Esta funcion solo es accesible dentro de logger.js
+const exampleFoo = ()=>{
+    console.log("Esta funcion no es accesible desde otro archivo")
+}
+
 /*
 const logger = require('./logger')
 logger.info('This is an informational message')
 logger.verbose('This is a verbose message')
-* */
+*/
