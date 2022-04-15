caedd4e6c385792f0430a3e75f1c54e528ef9f31
RPerales <69372903+pe-ca@users.noreply.github.com>
2022-04-15 18:30:18 -0500

    Delete weekly_mission_2/examples directory

--- weekly_mission_2/examples/ejemplo2.js
+++
@@ -1,7 +0,0 @@
-// Ejemplo 2: Crear un objeto con propiedades
-const myObjetc2 = {
-    name: "Carlo", //Creacion y asignacion de propiedades
-    age: 27
-}
-console.log("Ejemplo 2: Crear un objeto con propiedades")
-console.log(myObjetc2) //muestra las propiedades del objeto
