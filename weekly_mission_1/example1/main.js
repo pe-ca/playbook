d56d3ddcd6c5a4e1b4db59a3a557a350b6a77d65
pe-ca <mr.gloser1@gmail.com>
2022-04-15 12:32:17 -0500

    agregandas notas descriptivas

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -1,29 +1,28 @@
-// 1. Creación de un objeto con propiedades
-
-let myCar = new Object(); // Creación de un objeto
-myCar.make = 'Ford'; // Guardando un valor dentro del objeto creado
+console.log("Nota: Creación de un objeto con propiedades")
+// Creación de un objeto vacio
+let myCar = new Object();
+// Se agregan cada una de las propiedades que se desean en el objeto con el valor que van a contener, un objeto puede tener n cantidad de propiedades
+myCar.make = 'Ford';
 myCar.model = 'Mustang';
 myCar.year = 1969;
+// Imprimiendo objeto
+console.log(myCar)
+console.log("\n")
 
-console.log(myCar) // Imprimiendo objeto
 
-// 2. Declaración de un objeto con variables locales y públicas
-
+console.log("Nota: Declaración de un objeto con variables locales y públicas")
 const myModule = (() => {
-    // Variables de contexto local
+    // Variables declaradas dentro de un contexto, todas las variables que no sean expuestas a travez de un return seran privadas
     const privateFoo = "Soy un valor privado, solo me usan dentro de este objeto"
     const privateBar = [1,2,3,4]
     const baz = "Soy un valor que va a ser expuesto"
-
-    // Variable para guardar las variables locales
+    // Declaracion del objeto que se expondra a travez del return
     const exported = {
         publicFoo: "Valor público, pueden verme desde donde me llamen",
         publicBar: "Otro valor público",
         publicBaz: baz
     }
-
     // Exposición de variables locales
     return exported
 })()
-
 console.log(myModule)
