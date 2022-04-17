6b71068e38a55c60004d058e6ae3a2ab80c8bde3
pe-ca <mr.gloser1@gmail.com>
2022-04-17 14:02:19 -0500

    agregado codigo ejemplo y descripcion de ciclo foreach

---
+++ weekly_mission_2/2_clases_y_objetos/ejemplo1.js
@@ -0,0 +1,5 @@
+// Ejemplo 1: for Each para imprimir elementos de una lista
+//foreach es un ciclo que recorre y ejecuta la accion descrita por cada elemento de la lista que esta recorriendo
+const numbers = [1, 2, 3, 4, 5];
+console.log("Ejemplo 1: Imprimiendo los elementos de una lista")
+numbers.forEach(num => console.log(num))
