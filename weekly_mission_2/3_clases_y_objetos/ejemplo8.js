26b51a7535e7607ef6ebbfb425a949c26027204c
pe-ca <mr.gloser1@gmail.com>
2022-04-17 16:40:57 -0500

    ejemplo 8 uso de static en metodos

---
+++ weekly_mission_2/3_clases_y_objetos/ejemplo8.js
@@ -0,0 +1,20 @@
+// Ejemplo 8: Métodos static nos ayudan a escribir métodos en una clase que podemos usar sin necesidad de instanciar algún objeto
+// static permite acceder a los metodos que contienen esta palabra reservada sin necesidad de instanciar la clase
+class Toolbox {
+    static getMostUsefulTools(){
+        return ["Command line", "git", "Text Editor"]
+    }
+}
+
+console.log("Ejemplo 8: Métodos static")
+// Puedo llamar el método static directamente con el nombre de la clase
+console.log(Toolbox.getMostUsefulTools())
+// Si intentamos instanciar un objeto, no podremos llamar este método static
+//const toolbox = new Toolbox()
+//console.log(toolbox.getMostUsefulTools()) // is not a function
+
+/*
+
+HERENCIA: Nos permite relacionar clases entre sí y rehusar sus componentes
+
+*/
