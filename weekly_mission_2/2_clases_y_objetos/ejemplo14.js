20341f7a78dd9afdd25955353577e834c60ada51
RPerales <69372903+pe-ca@users.noreply.github.com>
2022-04-17 16:06:20 -0500

    Delete weekly_mission_2/2_clases_y_objetos directory

--- weekly_mission_2/2_clases_y_objetos/ejemplo14.js
+++
@@ -1,9 +0,0 @@
-// Ejemplo 14: Uso de some,
-// este método validará todos los elementos de la lista, y solo SI alguno cumple con la evaluacion
-// indicada, regresará true, de lo contrario será false.
-
-// lista de elementos
-const bools = [true, true, false, true]
-// Uso de Some para ver si al menos uno de los elementos es false
-const areSomeTrue = bools.some((b) =>  b === false)
-console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSomeTrue) //true
