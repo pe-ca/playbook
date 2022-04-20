42dfbae96a4073653681c218f3a699fb8cf8dad5
pe-ca <mr.gloser1@gmail.com>
2022-04-19 21:06:53 -0500

    name foreach

--- weekly_mission_2/exercises/ejercicio2/explorers.js
+++ weekly_mission_2/exercises/ejercicio2/explorers.js
@@ -51,3 +51,15 @@ const explorers = [
         }
     }
 ]
+
+
+/*Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH*/
+explorers.forEach(item => console.log(item.name))
+
+/*Imprime el stack de cada explorer, usa FOR EACH*/
+/*Crea una nueva lista con las listas de stacks de cada explorer, usa MAP*/
+/*Obtén la lista de explorers que tengan en su stack "js", usa FILTER (para validar un elemento en un lista se usa el método includes)*/
+/*Busca el primer explorer que sea de la CDMX, usa FIND*/
+/*Obtén la suma de todos los exercises_completed, usa REDUCE*/
+/*Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME*/
+/*Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY.*/
