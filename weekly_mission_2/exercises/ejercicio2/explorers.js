ca539f8622f2d41ea4ec24afd8aeaf25ad18c80e
pe-ca <mr.gloser1@gmail.com>
2022-04-19 21:24:55 -0500

    filter, include, map

--- weekly_mission_2/exercises/ejercicio2/explorers.js
+++ weekly_mission_2/exercises/ejercicio2/explorers.js
@@ -63,7 +63,10 @@ explorers.forEach(item => console.log(item.name + " " + item.stack))
 console.log("Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
 console.log(explorers.map((item)=> item.stack))
 
-/*Obtén la lista de explorers que tengan en su stack "js", usa FILTER (para validar un elemento en un lista se usa el método includes)*/
+console.log("Obtén la lista de explorers que tengan en su stack 'js', usa FILTER (para validar un elemento en un lista se usa el método includes)")
+console.log(explorers.filter(item => item.stack.includes("js")))
+console.log(explorers.filter(item => item.stack.includes("js")).map(item => item.name))
+
 /*Busca el primer explorer que sea de la CDMX, usa FIND*/
 /*Obtén la suma de todos los exercises_completed, usa REDUCE*/
 /*Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME*/
