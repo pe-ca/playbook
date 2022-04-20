9d8ae2c4072024ae51b03ec4415cbc156ed750f0
pe-ca <mr.gloser1@gmail.com>
2022-04-19 21:11:38 -0500

    stack foreach, name + stack

--- weekly_mission_2/exercises/ejercicio2/explorers.js
+++ weekly_mission_2/exercises/ejercicio2/explorers.js
@@ -53,10 +53,13 @@ const explorers = [
 ]
 
 
-/*Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH*/
+console.log("Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH")
 explorers.forEach(item => console.log(item.name))
 
-/*Imprime el stack de cada explorer, usa FOR EACH*/
+console.log("Imprime el stack de cada explorer, usa FOR EACH")
+explorers.forEach(item => console.log(item.stack))
+explorers.forEach(item => console.log(item.name + " " + item.stack))
+
 /*Crea una nueva lista con las listas de stacks de cada explorer, usa MAP*/
 /*Obtén la lista de explorers que tengan en su stack "js", usa FILTER (para validar un elemento en un lista se usa el método includes)*/
 /*Busca el primer explorer que sea de la CDMX, usa FIND*/
